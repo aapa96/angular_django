f5e545ebb056e4562019cc2026f59931a1b2d6cd
Alex Paredes <aapa96@gmail.com>
2020-02-04 11:24:06 -0500

    test

--- template/template/urls.py
+++ template/template/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html"),name="home")
+    path('angular/', TemplateView.as_view(template_name="angular.html"),name="Angular"),
+    path('react/', TemplateView.as_view(template_name="react.html"),name="React")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
