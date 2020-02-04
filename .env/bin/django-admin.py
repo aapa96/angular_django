88e8d54ef9276d0a795bb953612195a65b5b83b8
Alex Paredes <aapa96@gmail.com>
2020-02-04 11:06:19 -0500

    djangular template

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alexparedesaranzamendi/Desktop/code/angular_django/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- README.md
+++
@@ -1,2 +0,0 @@
-# angular_django
-Plantilla
